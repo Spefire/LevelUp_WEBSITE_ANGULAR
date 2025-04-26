f81750a6387ec24af98ab9bcf85ca238bdcb9a3d
Nicholas (Spefire) <spefire@gmail.com>
2025-04-26 13:04:56 +0200

    Fix modules and add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { CharacterStatsComponent } from './components/character-stats/character-stats.component';
-import { DailyActionsComponent } from './components/daily-actions/daily-actions.component';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CharacterStatsComponent, DailyActionsComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Next Level Up';
-}
+export class AppComponent {}
