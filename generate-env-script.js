ff5ce0097aa307c42e46ee68f83cdb8e9486045e
Nicholas (Spefire) <spefire@gmail.com>
2025-05-26 20:13:14 +0200

    Update Angular with prettier

--- generate-env-script.js
+++ generate-env-script.js
@@ -3,13 +3,6 @@ const fs = require('fs');
 const environmentContent = `
 export const environment = {
   production: true,
-  NG_APP_BO_ADMIN: '${process.env.NG_APP_BO_ADMIN}',
-  NG_APP_BO_MASTER: '${process.env.NG_APP_BO_MASTER}',
-  NG_APP_BO_PLAYER: '${process.env.NG_APP_BO_PLAYER}',
-  NG_APP_API_ANON_KEY: '${process.env.NG_APP_API_ANON_KEY}',
-  NG_APP_API_URL: '${process.env.NG_APP_API_URL}',
-  NG_APP_SUPABASE_ANON_KEY: '${process.env.NG_APP_SUPABASE_ANON_KEY}',
-  NG_APP_SUPABASE_URL: '${process.env.NG_APP_SUPABASE_URL}'
 };
 `;
 
