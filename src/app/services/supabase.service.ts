4f3dfb25703f2004c8ecf0f443a1f8d8e0c2aa0a
Nicholas (Spefire) <spefire@gmail.com>
2025-05-31 22:12:56 +0200

    Clean character

--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -14,6 +15,7 @@ import { environment } from 'src/environments/environment';
 export class SupabaseService {
   public session = new BehaviorSubject<Session | null>(null);
   private _supabase: SupabaseClient | null = null;
+  private _degubMode = true;
 
   // eslint-disable-next-line @typescript-eslint/member-ordering
   public session$ = this.session.asObservable();
@@ -27,8 +29,6 @@ export class SupabaseService {
     const url = environment.NG_APP_SUPABASE_URL || '';
     const key = environment.NG_APP_SUPABASE_ANON_KEY || '';
     this._supabase = createClient(url, key);
-
-    // Écouter les changements de session
     this._supabase.auth.onAuthStateChange((_event, session) => {
       // Connexion
       if (!this.session.value && session) {
@@ -65,15 +65,8 @@ export class SupabaseService {
   }
 
   public async getCharacter() {
-    console.log('----------------', this.user_id);
-    if (!this.user_id) return null;
-
-    console.log('getCharacter 1');
-    let result = await this._supabase.from('characters').select().eq('user_id', this.user_id).maybeSingle();
-    console.warn('result 1', result);
-    console.error('error 1', result?.error?.message);
+    let result = await this._requestGet('characters');
     if (!result.data) {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const character: any = {
         user_id: this.user_id,
         isAdmin: false,
@@ -81,15 +74,11 @@ export class SupabaseService {
         lastName: 'Super',
         firstName: 'Cookie',
       };
-      console.log('getCharacter 2');
-      result = await this._supabase.from('characters').insert(character).select();
-      console.warn('result 2', result);
-      console.error('error 2', result?.error?.message);
+      result = await this._requestPost('characters', character);
     }
 
-    if (!result.data) {
-      return null;
-    } else {
+    if (!result.data) return null;
+    else {
       const character: Character = {
         age: 22,
         avatar: result.data.avatar,
@@ -99,4 +88,46 @@ export class SupabaseService {
       return character;
     }
   }
+
+  private async _requestGet(target: string): Promise<any> {
+    const result = await this._supabase.from(target).select().eq('user_id', this.user_id).maybeSingle();
+    if (this._degubMode) console.log('requestGet', result);
+    if (result.error) console.error('requestGet', result.error.message);
+    return result.data;
+  }
+
+  /* private async _requestGetAll(target: string): Promise<any[]> {
+    const result = await this._supabase.from(target).select().eq('user_id', this.user_id);
+    if (this._degubMode) console.log('requestGetAll', result);
+    if (result.error) console.error('requestGetAll', result.error.message);
+    return result.data;
+  }*/
+
+  private async _requestPost(target: string, item: any): Promise<any> {
+    const result = await this._supabase.from(target).insert(item).select().single();
+    if (this._degubMode) console.log('requestPost', result);
+    if (result.error) console.error('requestPost', result.error.message);
+    return result.data;
+  }
+
+  /* private async _requestPostAll(target: string, items: any[]): Promise<any[]> {
+    const result = await this._supabase.from(target).insert(items).select();
+    if (this._degubMode) console.log('requestPostAll', result);
+    if (result.error) console.error('requestPostAll', result.error.message);
+    return result.data;
+  }
+
+  private async _requestPut(target: string, item: any): Promise<any> {
+    const result = await this._supabase.from(target).update(item).eq('id', item.id).select();
+    if (this._degubMode) console.log('requestPut', result);
+    if (result.error) console.error('requestPut', result.error.message);
+    return result.data;
+  }
+
+  private async _requestDelete(target: string, id: number | null): Promise<boolean> {
+    const result = await this._supabase.from(target).delete().eq('id', id).select();
+    if (this._degubMode) console.log('requestDelete', result);
+    if (result.error) console.error('requestDelete', result.error.message);
+    return true;
+  }*/
 }
