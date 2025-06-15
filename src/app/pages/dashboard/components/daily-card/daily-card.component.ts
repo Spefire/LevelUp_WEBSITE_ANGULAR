5fa6d6e45f47af97e51a25538480948f1426d874
Nicholas (Spefire) <spefire@gmail.com>
2025-06-15 21:24:52 +0200

    Dashbord fixes

--- src/app/pages/dashboard/components/daily-card/daily-card.component.ts
+++ src/app/pages/dashboard/components/daily-card/daily-card.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, DestroyRef, inject, input, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, input } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 import { ButtonComponent } from '@lucca-front/ng/button';
 import { IconComponent } from '@lucca-front/ng/icon';
 import { StatusBadgeComponent } from '@lucca-front/ng/statusBadge';
 
+import { QuestRewardsComponent } from '@src/components/quest-rewards/quest-rewards.component';
 import { Log } from '@src/models/logs.model';
 import { Quest } from '@src/models/quests.model';
 import { LogsService } from '@src/services/logs.service';
@@ -13,29 +14,24 @@ import { isSameDay } from '@src/utils/time';
 
 @Component({
   selector: 'daily-card',
-  imports: [CommonModule, ButtonComponent, IconComponent, StatusBadgeComponent],
+  imports: [CommonModule, ButtonComponent, IconComponent, StatusBadgeComponent, QuestRewardsComponent],
   templateUrl: './daily-card.component.html',
   styles: ':host { display: contents }',
 })
-export class DailyCardComponent implements OnInit {
+export class DailyCardComponent {
   public readonly currentDate = input.required<Date>();
   public readonly quest = input.required<Quest>();
   public readonly isOld = input.required<boolean>();
   public readonly isOptionnal = input.required<boolean>();
 
-  public logs: Log[];
   public logLinked: Log | null = null;
 
   private readonly _destroyRef = inject(DestroyRef);
 
   constructor(private _logsService: LogsService) {
     this._logsService.logs$.pipe(takeUntilDestroyed(this._destroyRef)).subscribe(logs => {
-      if (logs) this.logs = logs;
+      if (logs) this.logLinked = logs.find(log => this.quest().id === log.quest.id && isSameDay(this.currentDate(), new Date(log.date))) ?? null;
     });
-  }
-
-  public ngOnInit() {
-    this.logLinked = this.logs.find(log => this.quest().id === log.quest.id && isSameDay(this.currentDate(), new Date(log.date))) ?? null;
   }
 
   public completeQuest() {
